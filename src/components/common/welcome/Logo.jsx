3abfd589327ff7c3af62067bf27fa3cbc39a1300
musfiqurofficial <musfiqurofficial1999@gmail.com>
2023-04-15 04:55:26 +0600

    all code here

--- src/components/common/welcome/Logo.jsx
+++ src/components/common/welcome/Logo.jsx
@@ -6,22 +6,25 @@ import Globe3 from "./globe3.json";
 import "../../../App.css";
 
 const Logo = () => {
-  
   return (
-      <div className="mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl h-screen flex flex-col justify-center items-center ">
-        <img className="fade-in" src={logo} alt="" />
-        <div className="absolute lg:left-[42.5%] md:left-[33%] left-[31%] flex justify-center items-center fade-in">
-          <h1 className="lg:text-8xl md:text-7xl text-4xl font-bold logo-font text-[#666181] lg:-mr-12 -mr-5 md:-mr-10">
-            BNGL
-          </h1>
-          <Link to="/home">
-          <Lottie className="lg:w-56 md:w-44 w-24" animationData={Globe3} loop={true} />
-          </Link>
-          <h1 className="lg:text-8xl md:text-7xl text-4xl font-bold logo-font text-[#666181] lg:-ml-12 -ml-5 md:-ml-10">
-            BAL
-          </h1>
-        </div>
+    <div className="relative mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl h-screen flex flex-col justify-center items-center ">
+      <img className="fade-in" src={logo} alt="" />
+      <div className="absolute lg:left-[37%] md:left-[33%] left-[31%] flex justify-center items-center fade-in">
+        <h1 className="lg:text-8xl md:text-7xl text-4xl font-bold logo-font text-[#666181] lg:-mr-12 -mr-5 md:-mr-10">
+          BNGL
+        </h1>
+        <Link to="/home">
+          <Lottie
+            className="lg:w-56 md:w-44 w-24"
+            animationData={Globe3}
+            loop={true}
+          />
+        </Link>
+        <h1 className="lg:text-8xl md:text-7xl text-4xl font-bold logo-font text-[#666181] lg:-ml-12 -ml-5 md:-ml-10">
+          BAL
+        </h1>
       </div>
+    </div>
   );
 };
 
