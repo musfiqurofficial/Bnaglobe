901302bd2dcc51dafb3ad29cbccafb7aa1b61662
musfiqurofficial <musfiqurofficial1999@gmail.com>
2023-04-15 22:04:58 +0600

    done

--- src/components/common/welcome/FaithFirst.jsx
+++ src/components/common/welcome/FaithFirst.jsx
@@ -31,11 +31,11 @@ const FaithFirst = () => {
   return (
     <Link to="/bnglobal">
       <div className="mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl h-screen flex flex-col justify-center items-center">
-        <h1 className="text-6xl font-semibold uppercase fade-in text-[#A6A6A6A6]">
+        <h1 className="text-4xl sm:text-6xl font-semibold text-center uppercase fade-in text-[#A6A6A6A6]">
           Faith First
         </h1>
         {show && (
-          <h5 className="text-2xl mt-8 uppercase fade-in text-[#A6A6A6A6]">
+          <h5 className="text-2xl mt-4 sm:mt-8 text-center uppercase fade-in text-[#A6A6A6A6]">
             Financials Follow
           </h5>
         )}
